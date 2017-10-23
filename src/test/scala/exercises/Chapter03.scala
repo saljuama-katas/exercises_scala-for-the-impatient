79ab52db19ab9f943f7c662ab2bdaa68bac66ab6
Salvador Juan <sjuan@thoughtworks.com>
2017-10-24 00:27:44 +0200

    chapter 03 - exercise 11

--- src/test/scala/exercises/Chapter03.scala
+++ src/test/scala/exercises/Chapter03.scala
@@ -185,5 +185,29 @@ class Chapter03 extends FreeSpec with Matchers {
       }
     }
 
+    "Exercise 11" - {
+      """---------
+        |Import java.awt.datatransfer._ and make an object of type SystemFlavorMap with the call
+        |
+        |  val flavors = SystemFlavorMap.getDefaultFlavorMap().asInstanceOf[SystemFlavorMap]
+        |
+        |Then call the getNativesForFlavor method with parameter DataFlavor.imageFlavor and get
+        |the return value as a Scala buffer. (Why this obscure class? It’s hard to find uses of
+        |java.util.List in the standard Java library.)
+      """.stripMargin in {
+
+        import java.awt.datatransfer._
+
+        import collection.JavaConverters.asScalaBufferConverter
+        import scala.collection.mutable
+
+        val flavors = SystemFlavorMap.getDefaultFlavorMap.asInstanceOf[SystemFlavorMap]
+        val imageFlavors = flavors.getNativesForFlavor(DataFlavor.imageFlavor)
+
+        val scalaBuffer = imageFlavors.asScala
+
+        scalaBuffer should be(mutable.Buffer("PNG", "JFIF", "TIFF"))
+      }
+    }
   }
 }
