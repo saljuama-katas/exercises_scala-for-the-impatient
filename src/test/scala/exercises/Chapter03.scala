51b5df5aca034e27df01d21ab0c20a2063b4042f
Salvador Juan <sjuan@thoughtworks.com>
2017-10-23 22:40:29 +0200

    chapter 03 - exercise 1

---
+++ src/test/scala/exercises/Chapter03.scala
@@ -0,0 +1,30 @@
+package exercises
+
+import org.scalatest.{FreeSpec, Matchers}
+
+
+class Chapter03 extends FreeSpec with Matchers {
+
+  "Working with Arrays" - {
+
+    "Exercise 1" - {
+      """--------
+        |Write a code snippet that sets a to an array of n random integers between 0 (inclusive) and n (exclusive).
+      """.stripMargin in {
+
+        import util.Random
+
+        def randomArray(n: Int) = (for (_ <- 0 until n) yield Random.nextInt(n)).toArray
+
+        val a = randomArray(5)
+
+        a should have length 5
+        for (value <- a) {
+          value should be < 5
+          value should be >= 0
+        }
+      }
+    }
+
+  }
+}
