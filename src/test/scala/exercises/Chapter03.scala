cfc257f3816e5ef18e0b3a77161224cbb7497aec
Salvador Juan <sjuan@thoughtworks.com>
2017-10-23 23:24:51 +0200

    chapter 03 - exercise 2

--- src/test/scala/exercises/Chapter03.scala
+++ src/test/scala/exercises/Chapter03.scala
@@ -26,5 +26,24 @@ class Chapter03 extends FreeSpec with Matchers {
       }
     }
 
+    "Exercise 2" - {
+      """--------
+        |Write a loop that swaps adjacent elements of an array of integers. For example,
+        |Array(1, 2, 3, 4, 5) becomes Array(2, 1, 4, 3, 5).
+      """.stripMargin in {
+
+        val array = Array(1, 2, 3, 4, 5)
+
+        for (i <- array.indices by 2) {
+          if (i + 1 < array.length) {
+            val aux = array(i)
+            array(i) = array(i + 1)
+            array(i + 1) = aux
+          }
+        }
+
+        array should be(Array(2, 1, 4, 3, 5))
+      }
+    }
   }
 }
