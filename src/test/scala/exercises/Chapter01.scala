2425a90055b9b404b1f7442fec28e85575be4fb7
Salvador Juan <sjuan@thoughtworks.com>
2017-10-23 18:35:10 +0200

    chapter 01 - exercise 4

--- src/test/scala/exercises/Chapter01.scala
+++ src/test/scala/exercises/Chapter01.scala
@@ -42,6 +42,20 @@ class Chapter01 extends FreeSpec with Matchers {
       }
     }
 
+    "Exercise 4" - {
+      """--------
+        |Scala lets you multiply a string with a number, try out "crazy" * 3 in the REPL.
+        |What does this operation do? Where can you find it in Scaladoc?
+      """.stripMargin in {
+
+        val result = "crazy" * 3
+        result should be("crazycrazycrazy")
+
+        info(s"result = $result")
+        info("`StringOps` class has a method `*`")
+      }
+    }
+
 
   }
 }
