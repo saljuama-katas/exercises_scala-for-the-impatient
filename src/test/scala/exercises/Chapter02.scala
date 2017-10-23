386086923bb0d9103f17ca9d74f304f52ec7cfbe
Salvador Juan <sjuan@thoughtworks.com>
2017-10-23 22:19:02 +0200

    chapter 02 - exercise 11

--- src/test/scala/exercises/Chapter02.scala
+++ src/test/scala/exercises/Chapter02.scala
@@ -160,5 +160,37 @@ class Chapter02 extends FreeSpec with Matchers {
       }
     }
 
+    "Exercise 11" - {
+      """---------
+        |Define a string interpolator date so that you can define a java.time.LocalDate
+        |as date"$year-$month-$day". You need to define an “implicit” class with a date
+        |method, like this:
+        |
+        |  implicit class DateInterpolator(val sc: StringContext) extends AnyVal {
+        |    def date(args: Any*): LocalDate = ???
+        |  }
+        |
+        |args(i) is the value of the ith expression. Convert each to a string and then to
+        |an integer, and pass them to the LocalDate.of method. If you already know some
+        |Scala, add error handling. Throw an exception if there aren’t three arguments, or
+        |if they aren’t integers, or if they aren’t separated by dashes. (You get the
+        |strings in between the expressions as sc.parts.)
+      """.stripMargin in {
+
+        import java.time.LocalDate
+
+        implicit class DateInterpolator(val sc: StringContext) { //not value class because they can't be local
+          def date(args: Any*): LocalDate = {
+            val year = args(0).toString.toInt
+            val month = args(1).toString.toInt
+            val day = args(2).toString.toInt
+            LocalDate.of(year, month, day)
+          }
+        }
+
+        date"${2018}-${12}-${10}" should be(LocalDate.of(2018, 12, 10))
+      }
+    }
   }
 }
+
