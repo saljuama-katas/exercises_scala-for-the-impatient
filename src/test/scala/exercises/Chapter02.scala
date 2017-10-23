75f726de978bac8540f85302c9ceaf8e629645f3
Salvador Juan <sjuan@thoughtworks.com>
2017-10-23 20:59:12 +0200

    chapter 02 - exercise 3

--- src/test/scala/exercises/Chapter02.scala
+++ src/test/scala/exercises/Chapter02.scala
@@ -40,5 +40,20 @@ class Chapter02 extends FreeSpec with Matchers {
       }
     }
 
+    "Exercise 3" - {
+      """--------
+        |Come up with one situation where the assignment x = y = 1 is valid in Scala.
+        |(Hint: Pick a suitable type for x.)
+      """.stripMargin in {
+
+        var x = {}
+        var y = 0
+        x = y = 1
+
+        y should be(1)
+        x should be(())
+      }
+    }
+
   }
 }
