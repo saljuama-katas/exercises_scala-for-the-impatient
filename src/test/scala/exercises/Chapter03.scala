cb3f8e98b10e06eb9881bcaa34dc81733e03f73d
Salvador Juan <sjuan@thoughtworks.com>
2017-10-24 00:00:34 +0200

    chapter 03 - exercise 8

--- src/test/scala/exercises/Chapter03.scala
+++ src/test/scala/exercises/Chapter03.scala
@@ -125,5 +125,38 @@ class Chapter03 extends FreeSpec with Matchers {
       }
     }
 
+    "Exercise 8" - {
+      """--------
+        |Suppose you are given an array buffer of integers and want to remove all but
+        |the first negative number. Here is a sequential solution that sets a flag when
+        |the first negative number is called, then removes all elements beyond.
+        |
+        |  var first = true
+        |  var n = a.length
+        |  var i = 0
+        |  while (i < n) {
+        |    if (a(i) >= 0) i += 1
+        |    else {
+        |      if (first) { first = false; i += 1 }
+        |      else { a.remove(i); n -= 1 }
+        |    }
+        |  }
+        |
+        |This is a complex and inefficient solution. Rewrite it in Scala by collecting
+        |positions of the negative elements, dropping the first element, reversing the
+        |sequence, and calling a.remove(i) for each index.
+      """.stripMargin in {
+
+        import scala.collection.mutable.ArrayBuffer
+
+        val array = ArrayBuffer(1, 2, 3, -1, 4, 5, -2, 6, -7, -3, 7)
+
+        val negativeIndices = (for (i <- array.indices if array(i) <= 0) yield i).drop(1).reverse
+        for (index <- negativeIndices) array.remove(index)
+
+        array should be(ArrayBuffer(1, 2, 3, -1, 4, 5, 6, 7))
+      }
+    }
+
   }
 }
